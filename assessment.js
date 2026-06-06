d7420aed07ede395b7d9320d4b2be771d69d9a8b
Key-code-ai <otoja8881@gmail.com>
2026-06-06 19:31:33 +0900

    Add files via upload
    Bootstrap

--- assessment.js
+++ assessment.js
@@ -16,14 +16,28 @@ assessmentButton.addEventListener(
 
     // TODO 診断結果表示エリアの作成
     resultDivision.innerText = ''; //divタグを空文字で上書きすることで、空にしている
-    const header = document.createElement('h3'); // h3タグ作成
-    header.innerText = '診断結果'; //　タグの内側のテキストを設定
-    resultDivision.appendChild(header); //divタグの子要素として追加
+   
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
 
-    const paragraph = document.createElement('p')//pタグ作成
-    const result = assessment(userName);//診断結果作成
-    paragraph.innerText = result;//pタグの内側のテキスト
-    resultDivision.appendChild(paragraph);
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
     // TODO ツイートエリアの作成
     tweetDivision.innerText = '';// tweetのdivタグも空にする
     const anchor = document.createElement('a');
